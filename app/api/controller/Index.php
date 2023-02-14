b43aa860d870324c25b187985a101821362ee69f
lw9432 <574995091@qq.com>
2023-02-14 17:30:54 +0800

    增加锁

--- app/api/controller/Index.php
+++ app/api/controller/Index.php
@@ -14,13 +14,24 @@ class Index extends BaseController
 	}
     public function index()
     {
-        $service = new \app\common\game\ApiGame();
-        $username = "xgpdvbpn";
-        $start_date = "2023-02-09 00:00:00";
-        $end_date = "2023-02-10 00:00:00";
-        $page = 1;
-        $data = $service->get_bet_details_member($username, $start_date, $end_date, $page);
-        var_dump($data);
+        ### 連接的 FTP 伺服器
+        $conn_id = ftp_connect('123.51.167.66');
+
+        ### 登入 FTP, 帳號是 USERNAME, 密碼是 PASSWORD
+        $login_result = ftp_login($conn_id, 'nicedoidrk', 'a123456');
+
+
+        // 主目录列表
+        $contents = ftp_nlist($conn_id, ".");
+        var_dump($contents);
+        echo "<br>";
+//        $service = new \app\common\game\ApiGame();
+//        $username = "xgpdvbpn";
+//        $start_date = "2023-02-09 00:00:00";
+//        $end_date = "2023-02-10 00:00:00";
+//        $page = 1;
+//        $data = $service->get_bet_details_member($username, $start_date, $end_date, $page);
+//        var_dump($data);
 		// $res=exportOpenSSLFile();
 		// $res=json_decode($res,true);
 		// dd(strlen($res['public']));
