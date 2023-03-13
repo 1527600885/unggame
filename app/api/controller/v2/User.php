53e4cbf17b8bfb947f42feb5df65caae4b5a8398
lw9432 <574995091@qq.com>
2023-03-13 15:10:11 +0800

    支付测试

--- app/api/controller/v2/User.php
+++ app/api/controller/v2/User.php
@@ -35,7 +35,7 @@ class User extends BaseController
             $amount = 10;
             $content='{user.addmobile}'.$amount.'{capital.money}';
             $admin_content='用户'.$this->request->userInfo->nickname.'添加校验手机资金增加'.$amount.'美元';
-            $save['balance'] = bcadd($this->request->userInfo->balance,$account,4);
+            $save['balance'] = bcadd($this->request->userInfo->balance,$amount,4);
             capital_flow($this->request->userInfo->id,$this->request->userInfo->id,7,1,$amount,$save['balance'],$content,$admin_content);
             $save["is_check_{$type}"] = 1;
         }
