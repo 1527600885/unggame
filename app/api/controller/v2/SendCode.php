4f821393dd85280eca5492966c5df4f4bc47348d
lw9432 <574995091@qq.com>
2023-03-13 14:15:23 +0800

    支付测试

---
+++ app/api/controller/v2/SendCode.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace app\api\controller\v2;
+
+
+use app\api\BaseController;
+use app\api\model\User as UserModel;
+use think\facade\Validate;
+
+class SendCode extends BaseController
+{
+    public function index()
+    {
+        $account = input("post.account","");
+        $type = input("post.type","email");
+        $is_fill = input("post.is_fill",1);
+        $is_exit = input("post.is_exit",0);
+        if($type == "email")
+        {
+            if($is_fill && !Validate::is($account,"email"))
+            {
+                $this->error(lang("user.email"));
+                if($is_exit &&  UserModel::where("email",$account)->find())
+                {
+                    $this->error(lang("user.emailoccupy"));
+                }
+            }
+           return json(\app\api\addons\sendCode::email($account, 'index_bind_email_code', lang('user.bindemail')));
+        }else if($type == "mobile")
+        {
+            if($is_fill)
+            {
+                if(!Validate::is($account,"length:4,16|number"))
+                {
+                    $this->error(lang("user.mobileError"));
+                }
+                if($is_exit && UserModel::where("mobile",$account)->find())
+                {
+                    $this->error(lang("user.mobileexistence"));
+                }
+                $phone = '+'.input('uncode').$account;
+            }else{
+                $phone = '+'.$this->request->userInfo['uncode'].$this->request->userInfo['mobile'];
+            }
+             return json(\app\api\addons\sendCode::singleSend($phone));
+        }
+    }
+}
---
+++ app/api/controller/v2/User.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace app\api\controller\v2;
+
+
+use app\api\BaseController;
+use app\api\model\User as UserModel;
+use think\facade\Validate;
+
+class User extends BaseController
+{
+    public function checkAccount()
+    {
+        $account = input("post.account","");
+        $code = input("post.code","");
+        $is_fill = input("post.is_fill",0);
+        $type = input("post.type");
+        if(!$is_fill){
+            $account = UserModel::where("id",$this->request->userInfo['id'])->value($type);
+        }
+        $cache = cache($account);
+        if(!$cache || $code != $cache)
+        {
+            $this->error(lang("user.codeerror"));
+        }
+        if($is_fill && UserModel::where($type,$account)->find()){
+            $this->error($type == 'email' ? lang("user.emailoccupy"): lang("user.mobileexistence"));
+        }
+        $save = [
+            $type=>$account,
+            "update_time"=>date("Y-m-d H:i:s")
+        ];
+        if(!$this->request->userInfo["is_check_{$type}"]){
+            $amount = 10;
+            $content='{user.addmobile}'.$amount.'{capital.money}';
+            $admin_content='用户'.$this->request->userInfo->nickname.'添加校验手机资金增加'.$amount.'美元';
+            $save['balance'] = bcadd($this->request->userInfo->balance,$account,4);
+            capital_flow($this->request->userInfo->id,$this->request->userInfo->id,7,1,$amount,$save['balance'],$content,$admin_content);
+            $save["is_check_{$type}"] = 1;
+        }
+        $this->request->userInfo->save($save);
+        $data = $this->request->userInfo->toArray();
+        $data['pay_password'] = $data['pay_password']==0?0:1;
+        $data['password'] = '';
+        $this->success("Operation successful.",$data);
+    }
+}
