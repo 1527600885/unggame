1c2afc9225ac1a80f20e6016811eefe30cc10a2d
kuang <351755162@qq.com>
2023-03-14 23:36:19 +0800

    1

--- app/api/controller/Ung.php
+++ app/api/controller/Ung.php
@@ -52,7 +52,7 @@ class Ung extends BaseController
     		$ungsetdata['servicecharge'] = $ungone->servicecharge;//赎回手续费
     		$ungsetdata['redemptionprice'] = $ungone->redemptionprice;//赎回单价
     		$ungsetdata['balance'] = $userInfo['balance'];//用户余额
-    		$ungsetdata['buylimit'] = $userInfo['buylimit'];//最低购买额度
+    		$ungsetdata['buylimit'] = $ungone->buylimit;//最低购买额度
     // 		累计股息金额
             $userdvdall = Db::name("ung_user_divd")->where("userid",$userInfo['id'])->value("SUM(CAST(divdmoney as DECIMAL (18,2))) as divdmoney");
             if($userdvdall){
