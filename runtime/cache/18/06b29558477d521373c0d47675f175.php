673f55e0f51728cfdb064ab74a666d25800338ad
kuang <351755162@qq.com>
2023-03-15 11:12:42 +0800

    tj

--- runtime/cache/18/06b29558477d521373c0d47675f175.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:1:{i:0;a:4:{s:2:"id";i:3;s:5:"title";s:12:"图片水印";s:4:"name";s:9:"watermark";s:5:"value";s:254:"{"open":0,"type":"font","sizeType":"actual","scale":"15","position":"5","opacity":"100","image":"\/upload\/watermark.png?4476064","fontText":"OneKeyAdmin","fontFamily":"\/admin\/css\/fonts\/FZHTJW.ttf","fontSize":"28","fontAngle":0,"fontColor":"#C91818"}";}}
--- runtime/cache/cd/98e240fc802ef210e27ce17d9de6e4.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:1:{i:0;s:83:"/www/wwwroot/game.uswindltd.com/runtime/cache/f2/845c302b7219da37c16b8892ae440a.php";}
