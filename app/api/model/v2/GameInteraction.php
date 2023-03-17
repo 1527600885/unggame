95c519c8bd9cebba8a455715f5f45a4c638ea94c
lw9432 <574995091@qq.com>
2023-03-17 10:43:00 +0800

    支付测试

--- app/api/model/v2/GameInteraction.php
+++ app/api/model/v2/GameInteraction.php
@@ -9,14 +9,11 @@ use think\Model;
 class GameInteraction extends Model
 {
     protected $type = [
-        "update_at"=>"timestamp"
+        "updated_at"=>"timestamp"
     ];
     public function game()
     {
         return $this->belongsTo(GameList::class,"game_id");
     }
-    public function getUpdateAtAttr($value)
-    {
-        return date("Y-m-d H:i:s",$value);
-    }
+
 }
