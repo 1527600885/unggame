6a036bc6167b22a20e73653ab31bcd9f95051eca
lw9432 <574995091@qq.com>
2023-03-19 18:16:01 +0800

    loler

--- app/api/controller/v2/User.php
+++ app/api/controller/v2/User.php
@@ -119,10 +119,10 @@ class User extends BaseController
         $last = UserSign::where("user_id",$this->request->userInfo['id'])
             ->order("id desc")
             ->find();
-        if(!$last || $last['last_sign_time']+3600 < time()){
+        if(!$last || (($last['last_sign_time']+3600) < time())){
             $signData = ["canSign"=>1];
         }else{
-            $signData = ["canSign"=>0,["signTime"=>$last['last_sign_time']+3600 - time()]];
+            $signData = ["canSign"=>0,["signTime"=>($last['last_sign_time']+3600 - time())]];
         }
         $this->success("success",$signData);
     }
