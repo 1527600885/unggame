2f20db511986f53316d7168acc9f82aa4e71d52b
lw9432 <574995091@qq.com>
2023-03-21 16:47:29 +0800

    loler

--- public/plugins/top/menu.php
+++ public/plugins/top/menu.php
@@ -40,6 +40,40 @@ return array (
           ),
         ),
       ),
+      1 => 
+      array (
+        'title' => '排行游戏',
+        'sort' => 0,
+        'path' => 'mkTopGame/index',
+        'ifshow' => 0,
+        'logwriting' => 0,
+        'children' =>
+            array (
+                0 =>
+                    array (
+                        'title' => '查看',
+                        'path' => 'mkTopGame/index',
+                    ),
+                1 =>
+                    array (
+                        'title' => '发布',
+                        'path' => 'mkTopGame/save',
+                        'logwriting' => 1,
+                    ),
+                2 =>
+                    array (
+                        'title' => '编辑',
+                        'path' => 'mkTopGame/update',
+                        'logwriting' => 1,
+                    ),
+                3 =>
+                    array (
+                        'title' => '删除',
+                        'path' => 'mkTopGame/delete',
+                        'logwriting' => 1,
+                    ),
+            ),
+      ),
     ),
   ),
 );
