4824b215e686d0c165f040f1f6255fd64cc68c12
lw9432 <574995091@qq.com>
2023-04-11 14:03:59 +0800

    loler

--- app/api/model/v2/Announcement.php
+++ app/api/model/v2/Announcement.php
@@ -8,5 +8,16 @@ use think\Model;
 
 class Announcement extends Model
 {
-
+    public function getIconAttr($value,$data)
+    {
+        switch ($data['type']){
+            case 1:
+                $value = "/static/images/tips/notes.png";
+                break;
+            case 2:
+                $value = "/static/images/tips/notesq.png";
+                break;
+        }
+        return $value;
+    }
 }
