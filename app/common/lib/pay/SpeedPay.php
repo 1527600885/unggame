ea42e188123353fafed12ff00192b1fb9adabfd4
lw9432 <574995091@qq.com>
2023-04-15 09:22:30 +0800

    loler

---
+++ app/common/lib/pay/SpeedPay.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace app\common\lib\pay;
+
+
+class SpeedPay extends Pay
+{
+    protected $secret = "c44b0631bb6b4ae2a189f29951351e90";
+    protected $merchantId = "PM10178";
+    protected $channel="spay";
+    protected $callbackUrl = "/api/notify.speedpay/callback";
+    protected $apiUrl = "https://spayin.com";
+    public function run($type,$params)
+    {
+        $domain =  request()->domain();
+        $data = [
+            "callbackUrl" =>$domain.$this->callbackUrl,
+            "merchantId" =>$this->merchantId,
+            "merchantOrderId" =>$params['mch_order_no'],
+            "orderAmount" =>$params['trade_amount'],
+            "param" =>$this->currency_type,
+            "timeMillis" =>bcmul(microtime(true),1000)
+        ];
+        $sign = $this->getSign($data,$this->secret,"secret");
+        $data['sign'] = $sign;
+        $result_json = curl($this->apiUrl."/api/payment/recharge",$data);
+        $result = json_decode($result_json,true);
+        if($result['code'] == 1){
+            return ["orderNo"=>$params['mch_order_no'],"oriAmount"=>$params['trade_amount'],"payInfo"=>$result['data']];
+        }else{
+            echo $result_json;die();
+        }
+    }
+}
