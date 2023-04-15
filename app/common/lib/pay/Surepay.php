94cd75b2eadbc58a162e2c94244755e3befc79e6
lw9432 <574995091@qq.com>
2023-04-15 15:08:42 +0800

    loler

--- app/common/lib/pay/Surepay.php
+++ app/common/lib/pay/Surepay.php
@@ -15,6 +15,7 @@ class Surepay extends Pay
     {
         $domain =  request()->domain();
         $customer = "cust".time().rand(100,999);
+
         $data = [
             "merchant"=>$this->merchant,
             "amount"=>$params['trade_amount'],
@@ -30,25 +31,26 @@ class Surepay extends Pay
         ];
         $token = md5($data['merchant'].$data['amount'].$data['refid'].$data['customer'].$this->apikey.$this->currency_type."52.55.100.240");
         $data['token'] = $token;
+        $data2 = [
+            "merchant"=>$this->merchant,
+            "amount"=>$params['trade_amount'],
+            "refid"=>$params['mch_order_no'],
+            "customer"=>$customer,
+            "currency"=>$this->currency_type,
+            "bankcode"=>input("param.bankcode"),
+            "clientip"=>"52.55.100.240",
+            "post_url"=>$domain.$this->post_url,//回调地址,
+            "failed_return_url"=>$domain,
+            "return_url"=>$domain
+        ];
+        $data2['token'] = $token;
+        $result_json = curlNoIpSet($this->apiUrl."/fundtransfer",$data2);
+        echo $result_json;die();
         $result_json = curlNoIpSet($this->apiUrl."/payout",$data);
         $result = json_decode($result_json,true);
         if($result['status'] == 1)
         {
-            $data2 = [
-                "merchant"=>$this->merchant,
-                "amount"=>$params['trade_amount'],
-                "refid"=>$params['mch_order_no'],
-                "customer"=>$customer,
-                "currency"=>$this->currency_type,
-                "bankcode"=>input("param.bankcode"),
-                "clientip"=>"52.55.100.240",
-                "post_url"=>$domain.$this->post_url,//回调地址,
-                "failed_return_url"=>$domain,
-                "return_url"=>$domain
-            ];
-            $data2['token'] = $token;
-            $result_json = curlNoIpSet($this->apiUrl."/fundtransfer",$data2);
-            echo $result_json;die();
+
             $result = json_decode($result_json,true);
             return ["orderNo"=>$params['mch_order_no'],"oriAmount"=>$params['trade_amount'],"payInfo"=>$result['data']['payUrl']];
         }else{
