591aa79ea46f6f2acba7ef5506ad4118d2855364
lw9432 <574995091@qq.com>
2023-04-19 17:50:03 +0800

    loler

--- app/common/lib/pay/JmPay.php
+++ app/common/lib/pay/JmPay.php
@@ -7,9 +7,11 @@ namespace app\common\lib\pay;
 class JmPay extends Pay
 {
     private $channelId = "bdt02";
-    private $mchId = "1009525";
+//    private $mchId = "1009525";
+    private $mchId = "1008988";
     private $CallbackUrl = "/api/notify.jmpay/callback";
-    private $SecretKey = "cdDecdFDC86d1Ec0119976856Fb40c94087207A7cF83b5214ccd0180071b83a222970143aA52dD8169e56a2715577ddd";
+//    private $SecretKey = "cdDecdFDC86d1Ec0119976856Fb40c94087207A7cF83b5214ccd0180071b83a222970143aA52dD8169e56a2715577ddd";
+    private $SecretKey = "320B0303C39ac9FD3Ee1bA6b8ce31B558747413765Ac48587828Ea45B5e7233e0fa453d90585467A28694CFaC137584c";
     private $apiUrl = "https://web.9mpay.club";
     public  function run($type, $params)
     {
@@ -36,5 +38,27 @@ class JmPay extends Pay
         }else{
             throw new \Exception($result['message']);
         }
+    }
+    public function transfer($params)
+    {
+        $domain =  request()->domain();
+        $data = [
+            "accountNumber" =>$params['accountNumber'],
+            "channelId" =>$this->channelId,
+            "mchId" =>$this->mchId,
+            "notifyUrl" =>$domain.$this->CallbackUrl,
+            "paytype" =>"bdt",
+            "realname" =>$params['realname'],
+            "timestamp" =>time(),
+            "totalFee" =>$params['transfer_amount'],
+            "traceno" =>$params['mch_transferId'],
+            "userEmail" =>$params['userEmail'],
+            "userMobile" =>$params['userMobile']
+        ];
+        $sign = strtoupper(md5($this->getSign($data,$this->SecretKey)));
+        $data['sign'] = $sign;
+        $result_json = curl_json($this->apiUrl."/api/payment",$data);
+        echo $result_json;die();
+        $result = json_decode($result_json,true);
     }
 }
