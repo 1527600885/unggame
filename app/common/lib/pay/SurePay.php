fb4f081d7a7f8b25cfc7e6010fc30f30cfdb5174
lw9432 <574995091@qq.com>
2023-04-22 09:41:19 +0800

    loler

--- app/common/lib/pay/SurePay.php
+++ app/common/lib/pay/SurePay.php
@@ -30,10 +30,9 @@ class SurePay extends Pay
         ];
         $token = md5($data['merchant'].$data['amount'].$data['refid'].$data['customer'].$this->apikey.$this->currency_type."52.55.100.240");
         $data['token'] = $token;
-        $url = http_build_query($data);
-        return ["orderNo"=>$params['mch_order_no'],"oriAmount"=>$params['trade_amount'],"payInfo"=>$this->apiUrl."/fundtransfer?".$url];
-//        $result_json = curlNoIpSet($this->apiUrl."/fundtransfer",$data);
-//        echo $result_json;die();
+        $result_json = curlNoIpSet($this->apiUrl."/fundtransfer",$data);
+        $result_json = str_replace('action="fundauth"','action="'.$this->apiUrl.'/undauth"',$result_json);
+        echo $result_json;die();
 //        echo $result_json;die();
 //        $result_json = curlNoIpSet($this->apiUrl."/payout",$data);
 //        $result = json_decode($result_json,true);
