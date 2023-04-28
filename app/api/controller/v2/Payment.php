19a965b3557563e6d66e29a950e96697f399c7de
lw9432 <574995091@qq.com>
2023-04-28 16:11:15 +0800

    loler

--- app/api/controller/v2/Payment.php
+++ app/api/controller/v2/Payment.php
@@ -30,14 +30,44 @@ class Payment extends BaseController
             }
         }else{
             $list= \app\api\model\v2\Payment::field("id,name,url,type")->where(['is_show'=>1,'type'=>$type])->order('id asc')->select();
-            foreach ($list as $v)
-            {
-                $api = "https://api.binance.com/api/v3/ticker/price?symbol={$v['name']}USDT";
-                $result = json_decode(file_get_contents($api),true);
-                $data[] = ['name'=>$v,"rate"=>$result['price']];
-            }
+            $this->getCoinMarketCap();
+//            foreach ($list as $v)
+//            {
+//                $api = "https://api.binance.com/api/v3/ticker/price?symbol={$v['name']}USDT";
+//                $result = json_decode(file_get_contents($api),true);
+//                $data[] = ['name'=>$v,"rate"=>$result['price']];
+//            }
         }
         $this->success(lang('system.success'),$data);
+    }
+    public function getCoinMarketCap()
+    {
+        $url = 'https://sandbox-api.coinmarketcap.com/v2/tools/price-conversion';
+        $parameters = [
+            'amount' => '1',
+            "symbol" => 'USD',
+            'convert' => 'BTC,ETH,USDT'
+        ];
+
+        $headers = [
+            'Accepts: application/json',
+            'X-CMC_PRO_API_KEY: 9511b77d-1c7b-4875-b8a4-f8531580f328'
+        ];
+        $qs = http_build_query($parameters); // query string encode the parameters
+        $request = "{$url}?{$qs}"; // create the request URL
+
+
+        $curl = curl_init(); // Get cURL resource
+// Set cURL options
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $request,            // set the request URL
+            CURLOPT_HTTPHEADER => $headers,     // set the headers
+            CURLOPT_RETURNTRANSFER => 1         // ask for raw response instead of bool
+        ));
+
+        $response = curl_exec($curl); // Send the request, save the response
+        print_r(json_decode($response)); // print json decoded response
+        curl_close($curl); // Close request
     }
     public function cacheRate()
     {
