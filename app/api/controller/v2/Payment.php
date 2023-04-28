44ea38373ae92ef208bfb4f81245492bdcd0730d
lw9432 <574995091@qq.com>
2023-04-28 21:47:50 +0800

    loler

--- app/api/controller/v2/Payment.php
+++ app/api/controller/v2/Payment.php
@@ -30,49 +30,58 @@ class Payment extends BaseController
             }
         }else{
             $list= \app\api\model\v2\Payment::field("id,name,url,type")->where(['is_show'=>1,'type'=>$type])->order('id asc')->select();
-            $this->getCoinMarketCap();
-//            foreach ($list as $v)
-//            {
-//                $api = "https://api.binance.com/api/v3/ticker/price?symbol={$v['name']}USDT";
-//                $result = json_decode(file_get_contents($api),true);
-//                $data[] = ['name'=>$v,"rate"=>$result['price']];
-//            }
+
+            foreach ($list as $v)
+            {
+                $data[] = ['name'=>$v,"rate"=> $this->getCoinMarketCap()];
+            }
         }
         $this->success(lang('system.success'),$data);
     }
-    public function getCoinMarketCap()
+    public function getCoinMarketCap($type)
     {
-        $url = 'https://sandbox-api.coinmarketcap.com/v2/tools/price-conversion';
-        $parameters = [
-            'amount' => '1',
-            "symbol" => 'USD',
-            'convert' => 'BTC,ETH,USDT'
-        ];
+        $key = "virtualRate_{$type}";
+        $redis = (new Redis(['select'=>2]))->getRedis();
+        $rate = $redis->get($key);
+        if(!$rate){
+            $url = 'https://sandbox-api.coinmarketcap.com/v2/cryptocurrency/quotes/historical';
+            $parameters = [
+                'amount' => '1',
+                "symbol" => $type,
+                'convert' => 'USD'
+            ];
 
-        $headers = [
-            'Accepts: application/json',
-            'X-CMC_PRO_API_KEY: 9511b77d-1c7b-4875-b8a4-f8531580f328'
-        ];
-        $qs = http_build_query($parameters); // query string encode the parameters
-        $request = "{$url}?{$qs}"; // create the request URL
+            $headers = [
+                'Accepts: application/json',
+                'X-CMC_PRO_API_KEY: 9511b77d-1c7b-4875-b8a4-f8531580f328'
+            ];
+            $qs = http_build_query($parameters); // query string encode the parameters
+            $request = "{$url}?{$qs}"; // create the request URL
 
 
-        $curl = curl_init(); // Get cURL resource
-// Set cURL options
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $request,            // set the request URL
-            CURLOPT_HTTPHEADER => $headers,     // set the headers
-            CURLOPT_RETURNTRANSFER => 1         // ask for raw response instead of bool
-        ));
+            $curl = curl_init(); // Get cURL resource
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $request,            // set the request URL
+                CURLOPT_HTTPHEADER => $headers,     // set the headers
+                CURLOPT_RETURNTRANSFER => 1         // ask for raw response instead of bool
+            ));
 
-        $response = curl_exec($curl); // Send the request, save the response
-        print_r(json_decode($response,true)); // print json decoded response
-        curl_close($curl); // Close request
+            $response = curl_exec($curl); // Send the request, save the response
+            $data = json_decode($response,true); // print json decoded response
+            curl_close($curl); // Close request
+            if($data['status']['error_code'] == 0){
+                $rate = $data[0]['quote']['USD']['price'];
+                $redis->set($key,$rate,300);
+            }else{
+                throw new Exception($data['status']['error_message']);
+            }
+        }
+        return $rate;
     }
     public function cacheRate()
     {
         $key = "cashRateList";
-        $redis = (new Redis())->getRedis();
+        $redis = (new Redis(['select'=>2]))->getRedis();
         $rateList = $redis->get($key);
         if(!$rateList){
             $req_url = 'https://api.exchangerate.host/latest?base=USD';
