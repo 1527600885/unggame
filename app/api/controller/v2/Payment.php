bab663df071102147f5f291bdec35c35754b4cb3
lw9432 <574995091@qq.com>
2023-05-03 11:41:50 +0800

    loler

--- app/api/controller/v2/Payment.php
+++ app/api/controller/v2/Payment.php
@@ -16,83 +16,76 @@ class Payment extends BaseController
 {
     public function getWallet()
     {
-        $userInfo=$this->request->userInfo;
-        if($userInfo){
+        $userInfo = $this->request->userInfo;
+        if ($userInfo) {
             $data['balance'] = $userInfo->balance;
             // 今日股息
-            $data['dividend']=CapitalFlowmodel::where(['uid'=>$userInfo->id,'type'=>4,'money_type'=>1])->whereDay('add_time')->value("SUM(CAST(amount as DECIMAL (18,3))) as amount") ?? 0;
+            $data['dividend'] = CapitalFlowmodel::where(['uid' => $userInfo->id, 'type' => 4, 'money_type' => 1])->whereDay('add_time')->value("SUM(CAST(amount as DECIMAL (18,3))) as amount") ?? 0;
             // 总获得的股息
-            $data['dividends']=CapitalFlowmodel::where(['uid'=>$userInfo->id,'type'=>4,'money_type'=>1])->value("SUM(CAST(amount as DECIMAL (18,3))) as amount") ?? 0;
+            $data['dividends'] = CapitalFlowmodel::where(['uid' => $userInfo->id, 'type' => 4, 'money_type' => 1])->value("SUM(CAST(amount as DECIMAL (18,3))) as amount") ?? 0;
             //最小提现金额
-            $withdrawConfig =  ConfigModel::getVal('withdraw');
+            $withdrawConfig = ConfigModel::getVal('withdraw');
             $data['miniwithrawal'] = $withdrawConfig['minprice'];
             // 今日流水
-            $data['water']=round(GameBetLog::where(['user_id'=>$userInfo->id])->whereDay('betTime')->sum('betAmount'),2);
+            $data['water'] = round(GameBetLog::where(['user_id' => $userInfo->id])->whereDay('betTime')->sum('betAmount'), 2);
             //要显示的货币
-            $data['currency'] = CurrencyAll::where("is_show",1)->field("id,name,type,country,symbol,thumb_img")->select();
+            $data['currency'] = CurrencyAll::where("is_show", 1)->field("id,name,type,country,symbol,thumb_img")->select();
             $country = getipcountry($this->request->ip());
             $rateList = $this->cacheRate();
-            try{
-                foreach ($data['currency'] as $v){
-                    if($v['type'] == 2){
-                        $v['rate'] = $rateList[$v['name']];
-                    }else{
-                        $v['rate'] = bcdiv('1',$this->getCoinMarketCap('USD',$v['name']),8);
-                    }
-                    $v['amount'] = bcmul($v['rate'],$data['balance'],8);
-                    if(!empty($v['country']) && $v['country'] == $country)
-                    {
-                        $data['symbol'] = $v['symbol'];
-                        $data['country_amount']  = $v['amount'];
-                    }
-                    $v['rate'] = number_format($v['rate'],4);
+
+            foreach ($data['currency'] as $v) {
+                if ($v['type'] == 2) {
+                    $v['rate'] = $rateList[$v['name']];
+                } else {
+                    $v['rate'] = bcdiv('1', $this->getCoinMarketCap('USD', $v['name']), 8);
                 }
-                if(!isset($data['country_amount'])){
-                    $data['symbol'] = "$";
-                    $data['country_amount']  = $data['balance'];
+                $v['amount'] = bcmul($v['rate'], $data['balance'], 8);
+                if (!empty($v['country']) && $v['country'] == $country) {
+                    $data['symbol'] = $v['symbol'];
+                    $data['country_amount'] = $v['amount'];
                 }
-            }catch (\Exception $e){
-                var_dump($v['rate']);
+            }
+            if (!isset($data['country_amount'])) {
+                $data['symbol'] = "$";
+                $data['country_amount'] = $data['balance'];
             }
 
         }
-        $this->success(lang('system.operation_succeeded'),$data);
+        $this->success(lang('system.operation_succeeded'), $data);
 
     }
+
     public function getRate($type = 2)
     {
         $data = [];
-        if($type== 2)
-        {
-            $list= \app\api\model\v2\Payment::where(['is_show'=>1,'type'=>$type])->order('id asc')->column("currency_name");
+        if ($type == 2) {
+            $list = \app\api\model\v2\Payment::where(['is_show' => 1, 'type' => $type])->order('id asc')->column("currency_name");
             $currency_list = [];
-            foreach ($list as $v)
-            {
-                $currency_list = array_merge($currency_list,explode(",",$v));
+            foreach ($list as $v) {
+                $currency_list = array_merge($currency_list, explode(",", $v));
             }
 
             $rateList = $this->cacheRate();
-            foreach ($currency_list as $vv)
-            {
-                $data[] = ["name"=>$vv,"rate"=>$rateList[$vv]];
+            foreach ($currency_list as $vv) {
+                $data[] = ["name" => $vv, "rate" => $rateList[$vv]];
             }
-        }else{
-            $list= \app\api\model\v2\Payment::field("id,name,url,type")->where(['is_show'=>1,'type'=>$type])->order('id asc')->select();
+        } else {
+            $list = \app\api\model\v2\Payment::field("id,name,url,type")->where(['is_show' => 1, 'type' => $type])->order('id asc')->select();
 
-            foreach ($list as $v)
-            {
-                $v['rate'] =  $this->getCoinMarketCap($v['name']);
+            foreach ($list as $v) {
+                $v['rate'] = $this->getCoinMarketCap($v['name']);
                 $data[] = $v;
             }
         }
-        $this->success(lang('system.success'),$data);
+        $this->success(lang('system.success'), $data);
     }
-    public function getCoinMarketCap($type,$change = "USD")
+
+    public function getCoinMarketCap($type, $change = "USD")
     {
         $key = "virtualRate_{$type}_{$change}";
-        $redis = (new Redis(['select'=>2]))->getRedis();
+        $redis = (new Redis(['select' => 2]))->getRedis();
         $rate = $redis->get($key);
-        if(!$rate){
+        if (!$rate) {
             $url = 'https://pro-api.coinmarketcap.com/v2/tools/price-conversion';
             $parameters = [
                 'amount' => '1',
@@ -116,41 +109,42 @@ class Payment extends BaseController
             ));
 
             $response = curl_exec($curl); // Send the request, save the response
-            $data = json_decode($response,true); // print json decoded response
+            $data = json_decode($response, true); // print json decoded response
             //echo $response;die();
             curl_close($curl); // Close request
-            if($data['status']['error_code'] == 0){
+            if ($data['status']['error_code'] == 0) {
                 $rate = $data['data'][0]['quote'][$type]['price'];
-                $redis->set($key,$rate,300);
-            }else{
+                $redis->set($key, $rate, 300);
+            } else {
                 throw new Exception($data['status']['error_message']);
             }
         }
         return $rate;
     }
+
     public function cacheRate()
     {
         $key = "cashRateList";
-        $redis = (new Redis(['select'=>2]))->getRedis();
+        $redis = (new Redis(['select' => 2]))->getRedis();
         $rateList = $redis->get($key);
-        if(!$rateList){
+        if (!$rateList) {
             $req_url = 'https://api.exchangerate.host/latest?base=USD';
             $response_json = file_get_contents($req_url);
-            if(false !== $response_json) {
+            if (false !== $response_json) {
                 try {
-                    $response = json_decode($response_json,true);
-                    if($response['success'] === true){
+                    $response = json_decode($response_json, true);
+                    if ($response['success'] === true) {
                         $rateList = $response['rates'];
-                        $redis->set($key,json_encode($rateList),3600);
-                    }else{
+                        $redis->set($key, json_encode($rateList), 3600);
+                    } else {
                         throw new Exception("System error");
                     }
-                } catch(Exception $e) {
+                } catch (Exception $e) {
                     throw new Exception($e->getMessage());
                 }
             }
-        }else{
-            $rateList = json_decode($rateList,true);
+        } else {
+            $rateList = json_decode($rateList, true);
         }
         return $rateList;
 
