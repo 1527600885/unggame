54f9106ba0da93374437e0189ec1c2c67a46486e
lw9432 <574995091@qq.com>
2023-05-04 17:37:00 +0800

    loler

--- app/api/model/v2/GameList.php
+++ app/api/model/v2/GameList.php
@@ -1,9 +1,9 @@
 <?php
 
-
 namespace app\api\model\v2;
 
-
+use app\common\lib\Redis;
+use Hashids\Hashids;
 use think\Model;
 
 class GameList extends Model
@@ -32,5 +32,29 @@ class GameList extends Model
     public function getAuthorAttr($value)
     {
         return explode(",",$value);
+    }
+    public function getNicknameAttr($value,$data)
+    {
+        $redis = (new Redis())->getRedis();
+        $key = "bigwin_game_nickname_{$data['id']}";
+        $nickname = $redis->get($key);
+        if(!$nickname){
+            $hashids = new Hashids(env('hashids'), 6,env('hashids_write'));
+            $nickname = $hashids->encode($data['game_id']);
+            $redis->set($key,$nickname,600);
+        }
+        return $nickname;
+    }
+    public function getPriceAttr($value,$data)
+    {
+        $redis = (new Redis())->getRedis();
+        $key = "bigwin_game_price_{$data['id']}";
+        $price = $redis->get($key);
+        if(!$price){
+            $price = mt_rand(1000,1000000);
+            $price = bcmul($price,0.01,2);
+            $redis->set($key,$price,600);
+        }
+        return $price;
     }
 }
