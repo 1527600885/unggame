873a631dbfe9e18ca0b981c8c93454ec2006ffe9
lw9432 <574995091@qq.com>
2023-05-04 17:08:19 +0800

    loler

---
+++ app/api/controller/v3/Game.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace app\api\controller\v3;
+
+
+use app\api\BaseController;
+use app\api\model\v2\GameList;
+use think\facade\Cache;
+
+class Game extends BaseController
+{
+    protected $noNeedLogin = ['*'];
+
+    /**
+     *recommended slot Games
+     */
+    public function recommendedList()
+    {
+        $where[] = ['displayStatus', '=', 1];
+        $where[] = ['groom_sort', '>', 0];//排序序列号>0
+        $sort = "groom_sort asc";
+        $this->success("success", $this->getList($where, $sort));
+    }
+    public function interestingList()
+    {
+        $where[] = ['displayStatus', '=', 1];
+        $where[] = ['tablegame_sort', '>', 0];//排序序列号>0
+        $sort = "tablegame_sort asc";
+        $this->success("success", $this->getList($where, $sort));
+    }
+    public function getList($where, $sort)
+    {
+        $key = "gamelist_{$where}_{$sort}";
+        $lists = GameList::where($where)->order($sort)->cache($key)->select();
+        $count = count($lists);
+        $lists = array_chunk($lists, 4);
+        return compact("count", "lists");
+    }
+}
