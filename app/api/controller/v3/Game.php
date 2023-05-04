2df78fd7615359c8c04f14bcc8d8dc9923cba0af
lw9432 <574995091@qq.com>
2023-05-04 17:14:06 +0800

    loler

--- app/api/controller/v3/Game.php
+++ app/api/controller/v3/Game.php
@@ -32,7 +32,7 @@ class Game extends BaseController
     {
         $whereKey = json_encode($where);
         $key = "gamelist_{$whereKey}_{$sort}";
-        $lists = GameList::where($where)->order($sort)->cache($key)->select();
+        $lists = GameList::where($where)->order($sort)->cache($key)->select()->toArray();
         $count = count($lists);
         $lists = array_chunk($lists, 4);
         return compact("count", "lists");
