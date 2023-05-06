d858ee6c085a6ae57203c717bd441156921bae20
lw9432 <574995091@qq.com>
2023-05-06 15:28:26 +0800

    loler

---
+++ app/api/controller/v2/Withdrawal.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace app\api\controller\v2;
+
+
+use app\admin\model\Config as ConfigModel;
+use app\api\BaseController;
+use app\api\model\v2\UserIdcard;
+
+class Withdrawal extends BaseController
+{
+    public function getWithdrawalInfo()
+    {
+        $userInfo=$this->request->userInfo;
+        $useridcard = UserIdcard::where("user_id", $userInfo->id)->find();
+        if(!$useridcard || $useridcard['status']!=1){
+            $this->error('Real-name authentication required for account cash withdrawal.',"",412);
+        }
+        $withdrawConfig =  ConfigModel::getVal('withdraw');
+        $rate = $this->getRate($withdrawConfig);
+        $minPrice = $withdrawConfig['minprice'];
+        $this->success(lang('system.operation_succeeded'),compact("rate","minPrice"));
+    }
+    /**
+     * 获取后台配置的提现费率(要求邀请人数从小到大排列)
+     * @return int
+     */
+    public function getRate($withdrawConfig)
+    {
+
+        $invite_nume = $this->request->userInfo["invite_one_num"];
+        $rate = 0;
+        foreach ($withdrawConfig['tableData'] as $v){
+            if($invite_nume >= $v['number']){
+                $rate = $v['rate'];
+            }
+        }
+        return $rate;
+    }
+}
