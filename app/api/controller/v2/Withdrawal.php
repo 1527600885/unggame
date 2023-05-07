889c77328e8adf31a85768294b12abce299b19b9
lw9432 <574995091@qq.com>
2023-05-07 11:33:15 +0800

    loler

--- app/api/controller/v2/Withdrawal.php
+++ app/api/controller/v2/Withdrawal.php
@@ -6,8 +6,10 @@ namespace app\api\controller\v2;
 
 use app\admin\model\Config as ConfigModel;
 use app\api\BaseController;
+use app\api\model\CurrencyAll;
 use app\api\model\GameBetLog;
 use app\api\model\v2\UserIdcard;
+use app\api\model\WithdrawalSettings;
 
 class Withdrawal extends BaseController
 {
@@ -46,5 +48,14 @@ class Withdrawal extends BaseController
             }
         }
         return $rate;
+    }
+    public function getWithdrawalList()
+    {
+        $param = input("param.");
+        $currency = CurrencyAll::where("is_show", 1)->cache("currency_all_show",600)->field("id,name,type,country,symbol,thumb_img,url_list,payment_ids,withdrawl_ids")->select()->toArray();
+        $lists = array_column($currency,NULL,"name");
+        $data = $lists[$param['type']];
+        $withdrawalList = WithdrawalSettings::where("id","in",$data['withdrawl_ids'])->where("is_show",1)->field("id,name,image,min_amount,max_amount")->select();
+        $this->success(lang('system.operation_succeeded'),compact("withdrawalList"));
     }
 }
