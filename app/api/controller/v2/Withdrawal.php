238e0113b047e77dd7c8933b0bb45ddd564d8ec1
lw9432 <574995091@qq.com>
2023-05-07 15:14:57 +0800

    loler

--- app/api/controller/v2/Withdrawal.php
+++ app/api/controller/v2/Withdrawal.php
@@ -63,8 +63,8 @@ class Withdrawal extends BaseController
     {
         $setting = WithdrawalSettings::where("id",$settings_id)->field("id,other1")->find();
         $rateList = cacheRate();
-        $min_amount =  bcmul(ConfigModel::getVal('withdraw')['minprice'],$rateList[$type],8);
-        $max_amount = bcmul($this->request->userInfo['balance'],$rateList[$type],8);
+        $min_amount =  bcmul(ConfigModel::getVal('withdraw')['minprice'],$rateList[$type],4);
+        $max_amount = bcmul($this->request->userInfo['balance'],$rateList[$type],4);
         $setting['others'] = [];
         if($setting['other1'] && isset($setting['other1'][$type]))
         {
