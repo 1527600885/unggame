b2b4fa1c248a8897edf6841abfbee556f3628099
lw9432 <574995091@qq.com>
2023-05-07 14:23:29 +0800

    loler

--- app/api/model/CurrencyAll.php
+++ app/api/model/CurrencyAll.php
@@ -13,4 +13,10 @@ class CurrencyAll extends Model
     //
 	protected $name = 'currency_all';
 	protected $type = ['url_list'=>'json',"awards"=>"json"];
+	public static function getDataByName($name)
+    {
+        $currency = CurrencyAll::where("is_show", 1)->cache("currency_all_show", 600)->field("id,name,type,country,symbol,thumb_img,url_list,payment_ids,withdrawl_ids")->select()->toArray();
+        $lists = array_column($currency, NULL, "name");
+        return $lists[$name];
+    }
 }
