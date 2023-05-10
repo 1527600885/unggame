c76b4e9523620e08ca1825deb4a109ff76764350
lw9432 <574995091@qq.com>
2023-05-10 15:37:12 +0800

    loler

--- app/api/controller/notify/Wowpay.php
+++ app/api/controller/notify/Wowpay.php
@@ -42,14 +42,9 @@ class Wowpay extends Pay
             $wowPay = new \app\common\lib\pay\WowPay("");
             $key =  $wowPay->payConfig['debug'] ?  $wowPay->payConfig['testconfig'][$currency]['key'] : $wowPay->payConfig['config'][$currency]["key"];
             if($sign == $wowPay->getSign($result,$key)){
-                $withdrawl->online_status = $result['tradeResult'] == 1 ? 2 : 3;
-                $withdrawl->pay_time = time();
-                $withdrawl->save();
-                if( $withdrawl->online_status == 3){
-                    $userInfo =  User::where("id",$withdrawl->uid)->find();
-                    $content='{capital.withdrawalfailed}'.$withdrawl->amount.'{capital.money}';
-                    $admin_content='用户'.$userInfo->nickname.'提现失败,退款$'.$withdrawl->amount;
-                    capital_flow($withdrawl->uid,$withdrawl->id,11,1,$withdrawl->amount,$userInfo->balance,$content,$admin_content);
+                if($result['tradeResult'] == 1 || $result['tradeResult'] == 2){
+                    $online_status = $result['tradeResult'] == 1 ? 2 : 3;
+                    $this->updateTransferOrder($result['merTransferId'],$online_status);
                 }
             }
             echo "success";
