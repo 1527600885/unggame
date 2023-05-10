a62fb6b7affa89af73b44ce8358c0cc2510b9db8
lw9432 <574995091@qq.com>
2023-05-10 15:18:38 +0800

    loler

--- app/common/lib/pay/JmPay.php
+++ app/common/lib/pay/JmPay.php
@@ -10,6 +10,7 @@ class JmPay extends Pay
 //    private $mchId = "1009525";
     private $mchId = "1008988";
     private $CallbackUrl = "/api/notify.jmpay/callback";
+    private $transferback = "/api/notify.jmpay/transferback";
 //    private $SecretKey = "cdDecdFDC86d1Ec0119976856Fb40c94087207A7cF83b5214ccd0180071b83a222970143aA52dD8169e56a2715577ddd";
     public $SecretKey = "320B0303C39ac9FD3Ee1bA6b8ce31B558747413765Ac48587828Ea45B5e7233e0fa453d90585467A28694CFaC137584c";
     public $topayKey ="E83B078Ec5da7F840091D774a5d55736930c28baa81b861Bd3E7Ad4e76278BafcCF9883bDb2bb78E547123eA9Ed1363636C4bF9FfFae4b83D60E3225500D522D";
