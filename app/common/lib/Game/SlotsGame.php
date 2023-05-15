01687036ed13e89dd3e6e52a23cc3ef0c3d3eb35
lw9432 <574995091@qq.com>
2023-05-15 16:21:50 +0800

    loler

--- app/common/lib/Game/SlotsGame.php
+++ app/common/lib/Game/SlotsGame.php
@@ -8,6 +8,26 @@ class SlotsGame
 {
     protected $bet = 5;
     protected $total_win = 0;
+    protected $total_house = 0;
+    /*
+     * 1=城堡
+     * 2=苹果
+     * 3=金苹果
+     * 4=wild
+     * 5=公主
+     * 6=王子
+     * 7=王后
+     * 8=王
+     * 9=小矮人
+     * 10=A
+     * 11=K
+     * 12=Q
+     * 13=J
+     * 14=篮子
+     * 15=+
+     * 16=x
+     *
+     * */
     protected $goodsList = [
         1 => 5,//城堡
         2 => 8,//苹果
@@ -111,13 +131,69 @@ class SlotsGame
 
     ];
 
-    public function getResult()
+    public function getRespinResult($old, $bs)
+    {
+        $board = $old;
+        $list = [];
+        $list = array_pad($list, 30, 2);//放入30个红苹果
+        $list = array_pad($list, 32, 3);//放入2个金苹果
+        $list = array_pad($list, 33, 14);//放入篮子
+        $list = array_pad($list, 100, 0);//其余放空
+        shuffle($list);
+        $new_bs = [];
+        $bs_pos = [];
+        $basket = [];
+        foreach ($board as $k => &$v) {
+            foreach ($v as $kk => &$vv) {
+                if (!in_array($vv, [2, 3, 14])) {
+                    if (count($new_bs) < 3) {
+                        $re = array_pop($list);
+                        if ($re != 0) {
+                            $new_bs[] = [$k, $kk];
+                            $vv = $re;
+                            if ($re == 14) {
+                                $basket = [$k,$kk];
+                            }else{
+                                $bs[$k][$kk] = mt_rand(1,20)*0.5*$this->bet;
+                            }
+                        }
+                    }
+                } else {
+                    $bs_pos[] = [$k, $kk];//原来苹果火篮子的位置
+                }
+            }
+        }
+        $bs_count = count($new_bs) + count($bs_pos);
+        $total = 0;
+        foreach ($bs as $k=>$v){
+            foreach ($v as $kk=>$vv)
+            {
+                if($board[$k][$kk] == 12)
+                {
+                    $total += $vv;
+                }
+            }
+        }
+        if($basket){
+            $bs[$basket[0]][$basket[1]] = $total;
+        }
+        $extra_board = [];
+        $boadList = [];
+        for($i=0;$i<5;$i++)
+        {
+
+        }
+        return compact("board","bs_count","bs_pos","bs","");
+    }
+
+    public function getSpinResult()
     {
         $board = $this->getBoard();
         $winlines = $this->getWinLines($board);
         $bs = $this->getBs($board);
         $total_win = $this->total_win;
-        return compact("board","bs","winlines","total_win");
+        $total_house = $this->total_house;
+        return compact("board", "bs", "winlines", "total_win", "total_house");
     }
 
     public function getBs($result)
@@ -125,6 +201,9 @@ class SlotsGame
         foreach ($result as &$v) {
             foreach ($v as &$vv) {
                 if ($vv != 2 && $vv != 3) {
+                    if ($vv == 1) {
+                        $total_house += 1;
+                    }
                     $vv = 0;
                 } else {
                     $vv = mt_rand(1, 8) * 0.5 * $this->bet;
@@ -145,34 +224,12 @@ class SlotsGame
         shuffle($data);
         $result = [];
         for ($i = 0; $i < 5; $i++) {
-            $result[] = array_slice($data, $i * 5, 5);
+            $result[] = array_slice($data, $i * 4, 4);
 
         }
         return $result;
     }
-    //[
-    //  {
-    //    "amount": 75,
-    //    "line": 5,
-    //    "occurrences": 3,
-    //    "positions": [
-    //      [
-    //        0,
-    //        0
-    //      ],
-    //      [
-    //        1,
-    //        1
-    //      ],
-    //      [
-    //        2,
-    //        2
-    //      ]
-    //    ],
-    //    "symbol": 5,
-    //    "type": "lb"
-    //  }
-    //]
+
     public function getWinLines($result)
     {
         $winlines = [];
@@ -205,7 +262,7 @@ class SlotsGame
                 if (!$first) {
                     $first = 4;
                 }
-                $amounts = $this->rate[$first][$count]*$this->bet;
+                $amounts = $this->rate[$first][$count] * $this->bet;
                 $this->total_win += $amounts;
                 $winlines[] = ['amount' => $amounts, "line" => $k, "occurrences" => $count, "symbol" => $first, "positions" => $s, "type" => "lb"];
             }
