07598c52eb8afdd6b0223cc2785019c8c4f57eee
lw9432 <574995091@qq.com>
2023-06-12 10:51:34 +0800

    loler

--- app/api/validate/TeamApply.php
+++ app/api/validate/TeamApply.php
@@ -11,13 +11,15 @@ class TeamApply extends Validate
         "code"=>"require",
         "email"=>"require|email",
         "name"=>"require",
-        "type"=>"require|in:1,2"
+        "type"=>"require|in:1,2",
+        'surname'=>"require"
     ];
     protected $message = [
         "mobile.require"=>"Please fill in your mobile number",
         "email.require"=>"Please fill in your email",
         "email.email"=>"Email number format is incorrect",
-        "name"=>"Please fill in your name",
+        "name.require"=>"Please fill in your name",
+        "surname.require"=>"Please fill in your surname"
     ];
 
 }
